b9805939e0cf7665b9c62f72e61fe77a971391ce
Oliver Charles <ollie@ocharles.org.uk>
2017-10-07 18:08:50 +0100

    Correct haskell-overrides.nix to not point to my file system

--- haskell-overrides.nix
+++ haskell-overrides.nix
@@ -2,7 +2,15 @@ pkgs:
 {
   overrides =
     self: super: {
-      github = self.callPackage /home/ollie/work/github {};
       servant-github-webhook = pkgs.haskell.lib.doJailbreak super.servant-github-webhook;  
-    };
+
+      github = pkgs.haskell.lib.overrideCabal super.github (drv: {
+        src = pkgs.fetchgit {
+          url = "git://github.com/ocharles/github";
+          rev = "8312ba0b2a90823a2548cab4f112726933f8072a";
+          sha256 = "1k02hpls2cwrhp534w193g1ffzlsk3dlv42w3m6nf9x9xbnfvam4";
+        };
+        jailbreak = true;
+      });
+    }; 
 }
